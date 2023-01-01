414226dfa433f5ed58d78fa64c3c6a5e22226f88
Valso <warut92@gmail.com>
2023-01-01 14:06:16 +0700

    update

--- script.js
+++ script.js
@@ -22,36 +22,38 @@ function sercxi() {
   //filter array จาก textInput
   //[2] ค้นหา
   //เรียกข้อความที่จะค้นหา
-  let str_sxablono = document.getElementById("input").value;
+  let str_sxablono = document.getElementById("enigo").value;
 
   //ค้นหาข้อความด้วย regex
   //https://stackoverflow.com/a/50828436
-  let sxablono_regex = new RegExp(`${str_sxablono}`, "gi");
+  let sxablono_regex = new RegExp(`${str_sxablono}(?!,)`, "g");
   //https://www.delftstack.com/howto/javascript/javascript-filter-string/
   let rezulto = disigitaj_vortoj.filter(function (str) {
     return sxablono_regex.test(str);
   });
+  //การแสดงผลการค้นหาแบบตรงตัว
 
-  //[3] แสดงผล
+  //[3] เตรียมแสดงผล
   //สร้างข้อความ หากข้อความค้นหาว่างเปล่า
   if (str_sxablono === "") {
     rezulto = "ไม่พบคำค้นหา";
-    return document.getElementById("output").innerHTML = "<hr>" + rezulto;
+    return document.getElementById("eligo").innerHTML = "<hr>" + rezulto;
   }
 
-  //แปลงวัตถุ (rezulto) เป็นสตริง
+  //แปลงวัตถุ (rezulto) เป็นสตริง แล้วเปลี่ยนเครื่องหมายลูกน้ำที่ไม่มีอะไรตามหลังด้วย <br><hr>
   let sercxitaj_vortoj = rezulto.toString().replace(/,(?!\s)/g, "<br><hr>");
 
   //[4]ไฮไลท์คำใน sercxitaj_vortoj
   //hilight คำที่ค้นหา
   sercxitaj_vortoj = sercxitaj_vortoj.replace(
     new RegExp(`${str_sxablono}`, "gi"),
-    "<span class=\"border-highlight\">" + str_sxablono + "</span>"
+    "<b style=\"color:Blue\">" + str_sxablono + "</b>"
   );
-  
+
   //นำออกแสดงผล
   //if ถ้า sercxitaj_vortoj ไม่เท่ากับ "" ให้แสดงข้อความ ไม่พบคำค้นหา
+
   if (sercxitaj_vortoj !== "") {
-    document.getElementById("output").innerHTML = "<hr>" + sercxitaj_vortoj;
-  } else document.getElementById("output").innerHTML = "<hr>" + "ไม่พบคำค้นหา";
+    document.getElementById("eligo").innerHTML = "<hr>" + sercxitaj_vortoj;
+  } else  document.getElementById("eligo").innerHTML = "<hr>" + "ไม่พบคำค้นหา";
 }
