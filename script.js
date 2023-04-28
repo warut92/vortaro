49f598978fca221222ec786923157e652490d778
Valso <warut92@gmail.com>
2023-04-28 21:36:09 +0700

    granda ĝisdatiĝo

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ cxiuj_vortoj_HTML = cxiuj_vortoj_HTML.replace(/---/g, '')
 let disigitaj_vortoj = cxiuj_vortoj_HTML.split(/\n/g);
 let disigitaj_vortoj_al_kunmetitaj_vortoj = cxiuj_vortoj_HTML.split(/\n|;/g);
 disigitaj_vortoj_al_kunmetitaj_vortoj = disigitaj_vortoj_al_kunmetitaj_vortoj.filter(vorto => vorto !== "")
-console.log('DISIGITAJ_VORTOJ_AL_KUNMETITAJ_VORTOJ', disigitaj_vortoj_al_kunmetitaj_vortoj)
 
 //หา length โดยตัดช่องว่างออก
 let statistiko_disigitaj_vortoj = disigitaj_vortoj.filter(disigitaj_vortoj => disigitaj_vortoj !== "")
@@ -29,18 +28,42 @@ function sercxi() {
   }
   //filter array จาก textInput
   //[2] ค้นหา
+  //สร้าง string สำหรับใช้เป็น regex ค้นหา string ที่เริ่มต้นด้วยตัวอักษรนั้น ๆ (สำหรับอักษรละติน)
+  let komenclitero = ""
+  //สำหรับการค้นหาด้วยภาษาไทย
+  let porTajaSercxo = ""
+  //สำหรับการค้นหาด้วยอักษรละติน
+  let precizaSercxo = 0
   //เรียกข้อความที่จะค้นหา
   let str_sxablono = document.getElementById("enigo").value;
+  const str_sxablono_len = str_sxablono.length
+  console.log('STR_SXABLONO.CHARCODEAT(0)', str_sxablono.charCodeAt(0))
+  //การค้นหาด้วยภาษาไทยแบบตรงตัว
+  if (str_sxablono.charCodeAt(0) > 500) {
+    console.log("อักษรไทย")
+    document.getElementById('sercxoLingvo').innerHTML = "ค้นหาตรงตัว"
+    if (document.getElementById('checkbox').checked) {
+      komenclitero = " "
+      porTajaSercxo = "(,|;| |$)"
+    }
+  } else if (str_sxablono.charCodeAt(0) < 500) {
+    console.log("อักษรละติน")
+    document.getElementById('sercxoLingvo').innerHTML = "kapvorto"
+    if (document.getElementById('checkbox').checked) {
+      komenclitero = "^"
+      precizaSercxo = 1
+    }
+  }
+  str_sxablono = komenclitero + str_sxablono + porTajaSercxo
   //ถ้าตัวอักษรในช่องค้นหามากกว่า 1 ให้ดำเนินการต่อ
   if (str_sxablono.length > 1) {
     //ค้นหาข้อความด้วย regex
     //https://stackoverflow.com/a/50828436
-    let spaceto = " "
     let sxablono_regex = new RegExp(`${str_sxablono}`, "i");
     // let sxablono_regex = new RegExp(`${spaceto}${str_sxablono}(?!,)`, "g");
     console.log('SXABLONO_REGEX', sxablono_regex)
     //https://www.delftstack.com/howto/javascript/javascript-filter-string/
-    let rezulto = disigitaj_vortoj.filter(function (str) {
+    let rezulto = disigitaj_vortoj.filter(function(str) {
       //test() ส่งค่าเป็น boolean สำหรับการตรวจสอบการค้นหา
       return sxablono_regex.test(str);
     });
@@ -55,32 +78,51 @@ function sercxi() {
 
     //แปลงวัตถุ (rezulto) เป็นสตริง แล้วเปลี่ยนเครื่องหมายลูกน้ำที่ไม่มีอะไรตามหลังด้วย <br><hr>
     let sercxitaj_vortoj = rezulto.toString().replace(/,(?!\s)/g, "<br><hr>");
-    console.log('REZULTO', rezulto.length)
-
+    // console.log('REZULTO', rezulto.length)
 
     //[4]ไฮไลท์คำใน sercxitaj_vortoj
     //hilight คำที่ค้นหา
+    console.log('STR_SXABLONO', str_sxablono)
+
+    // อักษรไทย
+    console.log('(STR_SXABLONO.CHARCODEAT(0) > 500)', (str_sxablono.charCodeAt(0) > 500))
+    if (str_sxablono.charCodeAt(0) > 500) { //ลบ ^ ที่จะออกมาแสดงผล ในการค้นหาด้วยอักษรละติน
+      console.log('CONDITION PASSED')
+      str_sxablono = str_sxablono.substr(0)
+      console.log('STR_SXABLONO', str_sxablono)
+      // อักษรไทยแบบตรงตัว// ผ่าน
+    } else if (document.getElementById('checkbox').checked) {
+      console.log("แบบตรงตัว");
+      str_sxablono = str_sxablono.substr(precizaSercxo, str_sxablono_len + 1)
+      console.log('STR_SXABLONO_LEN', str_sxablono_len)
+      //อักษรไทยแบบไม่ตรงตัว
+    }
+      console.log("แบบไม่ตรงตัว");
+      str_sxablono = str_sxablono.substr(0)
+
     sercxitaj_vortoj = sercxitaj_vortoj.replace(
       new RegExp(`${str_sxablono}`, "gi"),
       "<b>" + str_sxablono + "</b>"
     );
+    console.log('STR_SXABLONO', str_sxablono)
 
     //นำออกแสดงผล
     //if ถ้า sercxitaj_vortoj ไม่เท่ากับ "" ให้แสดงข้อความ ไม่พบคำค้นหา
 
     if (sercxitaj_vortoj !== "") {
       document.getElementById("eligo").innerHTML = "<hr>" + sercxitaj_vortoj;
-    } else  document.getElementById("eligo").innerHTML = "<hr>" + "ไม่พบคำค้นหา";
+    } else document.getElementById("eligo").innerHTML = "<hr>" + "ไม่พบคำค้นหา";
   } else {
     document.getElementById("eligo").innerHTML = "";
   }
 
 }
-
 // Register service worker to control making site work offline
-//adoptita de https://github.com/mdn/pwa-examples/blob/main/a2hs/index.js
+//prenitaj de https://github.com/mdn/pwa-examples/blob/main/a2hs/index.js
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
+    .then(() => {
+      console.log('Service Worker Registered');
+    });
 }
