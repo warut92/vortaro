6b0519310940422a2b0966492ab30ffe58d65139
Valso <warut92@gmail.com>
2023-10-25 18:18:09 +0700

    ĝisdata

--- eksperimenta/skripto.js
+++ eksperimenta/skripto.js
@@ -1,26 +1,39 @@
+//preni JSON
 let cxiujVortoj = JSON.parse(datumo)
+console.log('CXIUJVORTOJ', cxiujVortoj)
 
+vortaro_HTML = vortaro_HTML.split(/<br>/g)
+
+console.log('VORTARO_HTML', vortaro_HTML)
+
+// forigi la apostrofojn
 let tekstoSenApos = document.getElementById("teksto").innerHTML.replace(/'/g, "")
 
+// eligi ilin sen apostrofoj
 document.getElementById("teksto").innerHTML = tekstoSenApos
 
+//funckigi per alklaki vortojn.
 document.addEventListener("click", function() {
-        var elektitaVorto = "";
+        let elektitaVorto = "";
         if (window.getSelection) {
             elektitaVorto = "^" + window.getSelection().toString() + "";
+            console.log('ELEKTITAVORTO', elektitaVorto)
         }
-        	console.log("sercxita ",elektitaVorto.slice(elektitaVorto.length - 3))
 
-        if (elektitaVorto.slice(elektitaVorto.length - 3) === "as ") {
-            elektitaVorto = elektitaVorto.slice(0, elektitaVorto.length - 3) + "i "
-        };
-        if (elektitaVorto.slice(elektitaVorto.length - 2) === "u ") {
-            elektitaVorto = elektitaVorto.slice(0, elektitaVorto.length - 2) + "i "
-        };
+        const sufiksoj = ["o", "a", "e", "i", "as", "is", "os"];
 
-        if (elektitaVorto.slice(elektitaVorto.length - 2) === "e ") {
-            elektitaVorto = elektitaVorto.slice(0, elektitaVorto.length - 2) + "a "
-        };
+        let radiko = elektitaVorto
+        let sufikso = ""
+        for (const s of sufiksoj) {
+          console.log('SUFIKSOJ', sufiksoj)
+          if (elektitaVorto.endsWith(s)) {
+            radiko = elektitaVorto.slice(0, -s.length)
+            console.log('RADIKO', radiko)
+            sufikso = s
+            console.log('SUFIKSO', sufikso)
+            break
+          }
+        }
 
         if (elektitaVorto !== "") {
             console.log("Selected text: " + elektitaVorto);
@@ -29,5 +42,69 @@ document.addEventListener("click", function() {
       		return sxablono_regex.test(str);
    		 });
       		console.log(rezulto)
+          if (elektitaVorto !== "^") {
+            document.getElementById('traduko').innerHTML = rezulto;
+          }
         }
+
     });
+
+    function analyzeEsperantoWord(word) {
+    const suffixes = [
+        "o", "-a", "-e", "-i", "-as", "-is", "-os",
+        "-is", "-us", "-u", "-on", "-an", "-aj", "-ajn"
+    ];
+
+    let root = word;
+    let suffix = "";
+
+    for (const s of suffixes) {
+        if (word.endsWith(s)) {
+            root = word.slice(0, -s.length);
+            suffix = s;
+            break;
+        }
+    }
+
+    return {
+        root: root,
+        suffix: suffix
+    };
+}
+
+function analyzeEsperantoWordWithPrefix(word) {
+    const prefixes = [
+        "mal", "ge", "ek", "dis", "re", "pre", "ek", "for"
+    ];
+
+    let prefix = "";
+    let root = word;
+    let suffix = "";
+
+    for (const p of prefixes) {
+        if (word.startsWith(p)) {
+            prefix = p;
+            root = word.slice(p.length);
+            const analysis = analyzeEsperantoWord(root);
+            root = analysis.root;
+            suffix = analysis.suffix;
+            break;
+        }
+    }
+
+    return {
+        prefix: prefix,
+        root: root,
+        suffix: suffix
+    };
+}
+
+// Example usage
+// const wordAnalysis = analyzeEsperantoWordWithPrefix("tenton");
+// console.log("Prefix:", wordAnalysis.prefix);
+// console.log("Root:", wordAnalysis.root);
+// console.log("Suffix:", wordAnalysis.suffix);
+
+const wordAnalysis = analyzeEsperantoWord("parolanto");
+console.log("Root:", wordAnalysis.root);
+console.log("Suffix:", wordAnalysis.suffix);
