1d3157c4e35265915fef8495215e2a2e02ec84e3
Valso <warut92@gmail.com>
2023-10-27 00:28:50 +0700

    aldoni {UL}

--- eksperimenta/skripto.js
+++ eksperimenta/skripto.js
@@ -1,110 +1,61 @@
-//preni JSON
-let cxiujVortoj = JSON.parse(datumo)
-console.log('CXIUJVORTOJ', cxiujVortoj)
-
+//preni variablon de th-vortaro.js, iom konverti al Array (tabeltipo)
+vortaro_HTML = vortaro_HTML.replace(/;/g, "<br>")
 vortaro_HTML = vortaro_HTML.split(/<br>/g)
-
-console.log('VORTARO_HTML', vortaro_HTML)
 
 // forigi la apostrofojn
 let tekstoSenApos = document.getElementById("teksto").innerHTML.replace(/'/g, "")
 
-// eligi ilin sen apostrofoj
+// eligi ilin sen apostrofoj.
 document.getElementById("teksto").innerHTML = tekstoSenApos
+
+//krei la veriablon por elektita vorto.
+let elektitaVorto = "";
+
 
 //funckigi per alklaki vortojn.
 document.addEventListener("click", function() {
-        let elektitaVorto = "";
         if (window.getSelection) {
             elektitaVorto = "^" + window.getSelection().toString() + "";
             console.log('ELEKTITAVORTO', elektitaVorto)
         }
+//krei la grupon de sufiksoj
+    const sufiksoj = [
+        "o", "a", "e",
+        "i", "as", "is", "os", "is", "us", "u",
+        "on", "an", "aj", "ajn",
+        "antoj"
+    ];
+// se la elektita vorto finiĝas per spaceto forigu ĝin!
+    if (elektitaVorto.endsWith(" ")) {
+      console.log(elektitaVorto.endsWith(" "));
+      elektitaVorto = elektitaVorto.substring(0, elektitaVorto.length - 1)
+    }
+//faru elektitan vorton kiel radikon.
+    let radiko = elektitaVorto;
 
-        const sufiksoj = ["o", "a", "e", "i", "as", "is", "os"];
+    console.log('ROOT', radiko)
+    let sufikso = "";
 
-        let radiko = elektitaVorto
-        let sufikso = ""
-        for (const s of sufiksoj) {
-          console.log('SUFIKSOJ', sufiksoj)
-          if (elektitaVorto.endsWith(s)) {
-            radiko = elektitaVorto.slice(0, -s.length)
-            console.log('RADIKO', radiko)
-            sufikso = s
-            console.log('SUFIKSO', sufikso)
-            break
-          }
+    for (const s of sufiksoj) {
+        if (elektitaVorto.endsWith(s)) {
+          console.log("ĉu vere? ",elektitaVorto.endsWith(s));
+            radiko = elektitaVorto.slice(1, - s.length);
+            console.log('ROOT', radiko)
+            sufikso = s;
+            console.log('SUFFIX', sufikso)
+
+            if (elektitaVorto !== "") {
+                console.log("Selected text: " + elektitaVorto);
+        		let sxablono_regex = new RegExp(`(${(radiko)})`, "ig");
+                let rezulto = vortaro_HTML.filter(function(str) {
+          		return sxablono_regex.test(str);
+       		 });
+          		console.log(rezulto)
+              if (elektitaVorto !== "^") {
+                document.getElementById('traduko').innerHTML = rezulto;
+              }
+            }
         }
-
-        if (elektitaVorto !== "") {
-            console.log("Selected text: " + elektitaVorto);
-    		let sxablono_regex = new RegExp(`(${(elektitaVorto)})`, "ig");
-            let rezulto = cxiujVortoj.filter(function(str) {
-      		return sxablono_regex.test(str);
-   		 });
-      		console.log(rezulto)
-          if (elektitaVorto !== "^") {
-            document.getElementById('traduko').innerHTML = rezulto;
-          }
-        }
+    }
 
     });
-
-    function analyzeEsperantoWord(word) {
-    const suffixes = [
-        "o", "-a", "-e", "-i", "-as", "-is", "-os",
-        "-is", "-us", "-u", "-on", "-an", "-aj", "-ajn"
-    ];
-
-    let root = word;
-    let suffix = "";
-
-    for (const s of suffixes) {
-        if (word.endsWith(s)) {
-            root = word.slice(0, -s.length);
-            suffix = s;
-            break;
-        }
-    }
-
-    return {
-        root: root,
-        suffix: suffix
-    };
-}
-
-function analyzeEsperantoWordWithPrefix(word) {
-    const prefixes = [
-        "mal", "ge", "ek", "dis", "re", "pre", "ek", "for"
-    ];
-
-    let prefix = "";
-    let root = word;
-    let suffix = "";
-
-    for (const p of prefixes) {
-        if (word.startsWith(p)) {
-            prefix = p;
-            root = word.slice(p.length);
-            const analysis = analyzeEsperantoWord(root);
-            root = analysis.root;
-            suffix = analysis.suffix;
-            break;
-        }
-    }
-
-    return {
-        prefix: prefix,
-        root: root,
-        suffix: suffix
-    };
-}
-
-// Example usage
-// const wordAnalysis = analyzeEsperantoWordWithPrefix("tenton");
-// console.log("Prefix:", wordAnalysis.prefix);
-// console.log("Root:", wordAnalysis.root);
-// console.log("Suffix:", wordAnalysis.suffix);
-
-const wordAnalysis = analyzeEsperantoWord("parolanto");
-console.log("Root:", wordAnalysis.root);
-console.log("Suffix:", wordAnalysis.suffix);
