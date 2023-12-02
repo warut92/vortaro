c0671e9fe463fab53871f04b2f3169a56da3451c
Valso <warut92@gmail.com>
2023-12-02 22:54:59 +0700

    ĝisdata dato

--- lasta_sxangxo.js
+++ lasta_sxangxo.js
@@ -1,1 +1,1 @@
- let lastaTempo = "2023-12-02 22:48:31";
+ let lastaTempo = "02-12-2023 22:48:31";
--- lasta_sxangxo.py
+++ lasta_sxangxo.py
@@ -5,7 +5,7 @@ dosierindiko = 'th-vortaro.js'
 modifinita_tempo = os.path.getmtime(dosierindiko)
 
 from datetime import datetime
-modifinita_tempo = datetime.fromtimestamp(modifinita_tempo).strftime('%Y-%m-%d %H:%M:%S')
+modifinita_tempo = datetime.fromtimestamp(modifinita_tempo).strftime('%d-%m-%Y %H:%M:%S')
 
 print(f" let lastaTempo = \"{modifinita_tempo}\";")
 
