7ee3922ea0cf56a9fd2b81e648325f16460bac95
Valso <warut92@gmail.com>
2024-02-26 17:24:02 +0700

    +aldoni novan opcion

---
+++ aliaj-fontoj.js
@@ -0,0 +1,14 @@
+document.addEventListener("click", function() {
+  if (window.getSelection) {
+    elektitaVorto = "" + window.getSelection().toString().toLowerCase();
+  }
+  // se la elektita vorto finiĝas per spaceto forigu ĝin!
+  if (elektitaVorto.endsWith(" ")) {
+    elektitaVorto = elektitaVorto.substring(0, elektitaVorto.length - 1)
+  }
+  console.log(elektitaVorto);
+  document.getElementById('aliaj-fontoj').innerHTML = "vidu :<i>“" + elektitaVorto + "”</i> en <a href=\"https://vortaro.net/#" + elektitaVorto + "\" target=\"_blank\">PIV</a>" + ", <a href=\"https://www.reta-vortaro.de/revo/dlg/index-2l.html?q=" + elektitaVorto + "\" target=\"_blank\">ReVo</a>" + ", <a href=\"https://eo.wikipedia.org/wiki/" + elektitaVorto + "\" target=\"_blank\">Wikipedio</a>";
+  if (elektitaVorto === "") {
+    document.getElementById('aliaj-fontoj').innerHTML = ""
+  }
+})
