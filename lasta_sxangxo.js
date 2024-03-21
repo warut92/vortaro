8e3b8cf40d1331fdb457c96454b804dee74a7079
Valso <warut92@gmail.com>
2024-03-21 18:44:40 +0700

    trd

--- lasta_sxangxo.js
+++ lasta_sxangxo.js
@@ -1,2 +1,2 @@
-Eraro okazis: 'charmap' codec can't encode character '\u016d' in position 32: character maps to <undefined>
-Redaktitaj vortoj: 58
+Eraro okazis: 'charmap' codec can't encode character '\u016d' in position 31: character maps to <undefined>
+Redaktitaj vortoj: 1
