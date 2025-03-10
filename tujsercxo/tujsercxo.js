7210666667c6e40082cae09c7cd2d5f007033d03
Valso <warut92@gmail.com>
2025-03-10 08:21:32 +0700

    cimo

--- tujsercxo/tujsercxo.js
+++ tujsercxo/tujsercxo.js
@@ -1,6 +1,7 @@
 let i = 1
 let enigo = document.getElementById('enigo')
 let pivDiv = document.getElementById('piv_vortoj')
+
 function tujsercxi() {
     if (i == 1) {
         pivDiv.style.display = "block"
@@ -13,20 +14,22 @@ function tujsercxi() {
 }
 
 function PIVtujsercxi() {
-    let str_sxablono = enigo.value
-
-    piv_vortoj = piv_vortoj.replace(/   /g, "+")
-    const PIV_VORTOJ = piv_vortoj.split("\n")
-
-    //regex
-    let sxablono_regex = new RegExp(`(${(str_sxablono)})`, "gmi");
-    let REZULTO = PIV_VORTOJ.filter(function(str) {
-      //test() ส่งค่าเป็น boolean สำหรับการตรวจสอบการค้นหา
-      return sxablono_regex.test(str);
-    });
-    const REZULTO_JOIN = REZULTO.join("</span><span onclick=\"preniPIVvortojn(this)\">,");
-    let rezulto = REZULTO_JOIN.toString().replace(/^<\/span>/g,"").replace(/,/g,"<br>")
-   pivDiv.innerHTML = "<span onclick=\"preniPIVvortojn(this)\">" + rezulto
+    if (i == 0) {
+        let str_sxablono = enigo.value  
+            //piv_vortoj venas de la dosiero piv_vortoj.js    
+            const PIV_VORTOJ = piv_vortoj.split("\n")
+            piv_vortoj = piv_vortoj.replace(/   /g, "+")
+        
+            //regex
+            let sxablono_regex = new RegExp(`(${(str_sxablono)})`, "gmi");
+            let REZULTO = PIV_VORTOJ.filter(function(str) {
+              //test() ส่งค่าเป็น boolean สำหรับการตรวจสอบการค้นหา
+              return sxablono_regex.test(str);
+            });
+            const REZULTO_JOIN = REZULTO.join("</span><span onclick=\"preniPIVvortojn(this)\">,");
+            let rezulto = REZULTO_JOIN.toString().replace(/^<\/span>/g,"").replace(/,/g,"<br>")
+           pivDiv.innerHTML = "<span onclick=\"preniPIVvortojn(this)\">" + rezulto
+    }
 }
 
 function preniPIVvortojn(elektita_piv_vorto) {
