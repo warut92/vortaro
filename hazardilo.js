a5b2c6b6e7d948d95122efecf6c1aedc94345e1c
Valso <warut92@gmail.com>
2025-03-12 08:56:38 +0700

    cimo

---
+++ hazardilo.js
@@ -0,0 +1,8 @@
+console.log(tuta_vortaro_senspaceto);
+function hazardaVorto() {
+    for (let i = 0; i < tuta_vortaro_senspaceto.length; i++) {
+        const hazarda_vorto = tuta_vortaro_senspaceto[i];
+        console.log(hazarda_vorto);
+    }
+}
+hazardaVorto()
